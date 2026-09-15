ff795282d2a20abca7b484a8a80e209d01ad4dee
fernando-churros <fernando_churros@outlook.com>
2026-09-15 05:58:49 -0300

    refactor(task-tracker): adicionar atributos privados e métodos estáticos para id e data

--- python/task-tracker/CreateTask.py
+++ python/task-tracker/CreateTask.py
@@ -1,35 +1,73 @@
+from datetime import datetime
 from typing import Any
-
+from dados import all_tasks
 
 class CreateTask:
-    def __init__(self, id_task:int, name: str, desc: str, create_date, status: str = 'todo') -> None:
-        self.id_task = id_task
+    def __init__(self, name: str, desc: str) -> None:
+        self.__id_task = self.create_id()
         self.name = name
         self.desc = desc
-        self.create_date = create_date
-        self.status = status
+        self.__create_date = self.create_date()
+        self.status = 'todo'
 
     @property
-    def create_date(self) -> Any:
-        return self._create_date
-    @create_date.setter
-    def create_date(self, date) -> None:
-        self._create_date = date
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, item):
+        (is_valid, name) = self.validar_input(item)
+
+        if not is_valid:
+            raise ValueError('Nome não pode ser vazio')
+
+        self._name = name
 
     @property
-    def id_task(self) -> Any:
-        return self._id
-    @id_task.setter
-    def id_task(self, id_task):
-        self._id = id_task
+    def desc(self):
+        return self._desc
+    @desc.setter
+    def desc(self, item):
+        (is_valid, desc) = self.validar_input(item)
+
+        if not is_valid:
+            raise ValueError('Descrição não pode ser vazia')
+
+        self._desc = desc
+
+    @staticmethod
+    def create_id() -> int:
+        tasks = all_tasks()
+        if len(tasks) != 0:
+            id_task = max(task['id'] for task in tasks) + 1
+        else:
+            id_task = 1
+
+        return id_task
+
+    @staticmethod
+    def create_date():
+        x: datetime = datetime.now()
+        create_date = x.strftime('%d/%m/%y ás %H:%M')
+
+        return create_date
 
     def to_dict(self) -> dict:
         to_dict = {
-            'id': self.id_task,
+            'id': self.__id_task,
             'nome': self.name,
             'descrição': self.desc,
-            'data de criação': self.create_date,
+            'data de criação': self.__create_date,
             'status': self.status
         }
 
         return to_dict
+
+    @staticmethod
+    def validar_input(item) -> Any:
+        input_user = item.strip()
+        input_user = ' '.join(input_user.split())
+
+        if input_user == '':
+            return False, input_user
+
+        return True, input_user
