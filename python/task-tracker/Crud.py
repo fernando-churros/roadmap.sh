b24e16eaf42a431fcc9d93f66a184f3b0c9c148e
fernando-churros <fernando_churros@outlook.com>
2026-09-15 05:59:25 -0300

    refactor(task-tracker): simplificar classe Crud utilizando módulo dados

--- python/task-tracker/Crud.py
+++ python/task-tracker/Crud.py
@@ -1,36 +1,12 @@
 import json
-from datetime import datetime
-from CreateTask import CreateTask
-
+from dados import all_tasks, file_json
 
 class Crud:
-    FILE_JSON = 'dados.json'
-
     def __init__(self):
-        self.tasks = self.all_tasks()
+        self.tasks = all_tasks()
 
-    def add(self, name, desc) -> None:
-        x: datetime = datetime.now()
-        create_date = x.strftime('%d/%m/%y ás %H:%M')
-        if len(self.tasks) != 0:
-            id_task = max(task['id'] for task in self.tasks) + 1
-        else:
-            id_task = 1
+    def add(self, other) -> None:
+        self.tasks.append(other.to_dict())
 
-        new_task = CreateTask(id_task, name, desc, create_date)
-        self.tasks.append(new_task.to_dict())
-
-        with open(self.FILE_JSON, 'w', encoding='utf-8') as f:
+        with open(file_json, 'w', encoding='utf-8') as f:
             json.dump(self.tasks, f, indent=2, ensure_ascii=False)
-
-    def all_tasks(self) -> list:
-        try:
-            with open(self.FILE_JSON, "r", encoding="utf-8") as f:
-                x = json.load(f)
-
-        except FileNotFoundError:
-            with open(self.FILE_JSON, "w", encoding="utf-8") as f:
-                json.dump([], f, indent=2, ensure_ascii=False)
-            x = []
-
-        return x
