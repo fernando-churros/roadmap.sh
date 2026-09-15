ec9fc56748ef92accc247759ccaf939b5ac7dac5
fernando-churros <fernando_churros@outlook.com>
2026-09-15 17:41:13 -0300

    feat(task-tracker): implementar operações de remoção, atualização de status, busca e listagem no Crud

--- python/task-tracker/Crud.py
+++ python/task-tracker/Crud.py
@@ -1,12 +1,110 @@
 import json
-from dados import all_tasks, file_json
+from CreateTask import CreateTask
 
 class Crud:
+    file_json = 'dados.json'
+
     def __init__(self):
-        self.tasks = all_tasks()
+        self.tasks = self.all_tasks()
 
     def add(self, other) -> None:
-        self.tasks.append(other.to_dict())
+        other.id = self.create_id()
+        self.tasks.append(other)
 
-        with open(file_json, 'w', encoding='utf-8') as f:
-            json.dump(self.tasks, f, indent=2, ensure_ascii=False)
+    def delete(self, value):
+        is_deleted:bool = False
+
+        for item in self.tasks:
+            if item.id == value:
+                self.tasks.remove(item)
+                is_deleted = True
+                break
+
+        if not is_deleted:
+            raise ValueError('Tarefa não encontrada')
+
+    def update(self, value:int, new_status:int):
+        (is_valid, task) = self.search_id(value)
+
+        if not is_valid:
+            raise ValueError('Tarefa não encontrada')
+
+        status:str = 'todo'
+        match new_status:
+            case 1:
+                pass
+            case 2:
+                status = 'in-progress'
+            case 3:
+                status = 'done'
+
+        task.update_status(status)
+
+    def list_all(self):
+        x = []
+        for other in self.tasks:
+            x.append(other)
+        return x
+
+    def list_one(self, value) -> object:
+        is_valid, task = self.search_id(value)
+        return task
+
+    def search_id(self, value) -> tuple[bool, object]:
+        is_search = False
+
+        task = None
+        for x in self.tasks:
+            if x.id == value:
+                task = x
+                is_search = True
+                break
+
+        if task is None:
+            raise RuntimeError('Tarefa não localizada')
+
+        return is_search, task
+
+    def write_json(self):
+        json_file = []
+        for x in self.tasks:
+            json_file.append(x.to_dict())
+
+        with open(self.file_json, 'w', encoding='utf-8') as f:
+            json.dump(json_file, f, indent=2, ensure_ascii=False)
+
+    def create_id(self) -> int:
+        if len(self.tasks) != 0:
+            id_task = max(task.id for task in self.tasks) + 1
+        else:
+            id_task = 1
+
+        return id_task
+
+    def all_tasks(self) -> list:
+        try:
+            list_object = []
+
+            with open(self.file_json, "r", encoding="utf-8") as f:
+                x = json.load(f)
+
+            if len(x) != 0:
+                for y in x:
+                    i = CreateTask(
+                        y['nome'],
+                        y['descrição'],
+                        y['id'],
+                        y['data de criação'],
+                        y['ultima modificação'],
+                        y['status']
+                    )
+
+                    list_object.append(i)
+
+            return list_object
+        except FileNotFoundError:
+            with open(self.file_json, "w", encoding="utf-8") as f:
+                json.dump([], f, indent=2, ensure_ascii=False)
+            x = []
+
+        return x
