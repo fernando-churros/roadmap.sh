6e7d0986a4c1ed593158aa0b4ec00136e0de0212
fernando-churros <fernando_churros@outlook.com>
2026-09-15 06:00:20 -0300

    feat(task-tracker): ajustar loop interativo com fluxo contínuo e opção de encerramento

--- python/task-tracker/__main__.py
+++ python/task-tracker/__main__.py
@@ -1,8 +1,26 @@
+from Menu import Menu
 from Crud import Crud
+from CreateTask import CreateTask
 
 def main():
-    c = Crud()
-    c.add('cli python', 'todo task in python cli')
+    while True:
+        try:
+            menu = Menu()
+            option = menu.initial()
+
+            match option:
+                case 1:
+                    menu.create()
+                    task = CreateTask(str(input('Nome da tarefa: ')), str(input('Descrição da tarefa: ')) )
+                    Crud().add(task)
+
+                    print('Tarefa criada')
+                case 5:
+                    print('Saindo...')
+                    break
+
+        except ValueError as e:
+            print(e)
 
 if __name__ == "__main__":
     main()
