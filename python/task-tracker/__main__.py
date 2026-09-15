213f2ef2980f387313ec4308771f11706d836cc1
fernando-churros <fernando_churros@outlook.com>
2026-09-15 17:42:04 -0300

    feat(task-tracker): integrar operações de remoção, atualização e listagem no loop principal

--- python/task-tracker/__main__.py
+++ python/task-tracker/__main__.py
@@ -3,6 +3,7 @@ from Crud import Crud
 from CreateTask import CreateTask
 
 def main():
+    crud = Crud()
     while True:
         try:
             menu = Menu()
@@ -12,14 +13,43 @@ def main():
                 case 1:
                     menu.create()
                     task = CreateTask(str(input('Nome da tarefa: ')), str(input('Descrição da tarefa: ')) )
-                    Crud().add(task)
+                    crud.add(task)
 
                     print('Tarefa criada')
+                case 2:
+                    menu.delete()
+                    item = int(input('id: '))
+                    crud.delete(item)
+
+                    print('Tarefa deletada')
+                case 3:
+                    menu.update()
+                    item = int(input('id: '))
+                    new_status = int(input('status: '))
+                    crud.update(item, new_status)
+                    print('Tarefa atualizada')
+                case 4:
+                    menu.to_list()
+                    option = int(input('Opção: '))
+
+                    match option:
+                        case 1:
+                            item = crud.list_one(int(input('ID: ')))
+                            for key, value in item.to_dict().items():
+                                print(f'{key}: {value}')
+                            print('-' * 50)
+                        case 2:
+                            item = crud.list_all()
+                            for other in item:
+                                for key, value in other.to_dict().items():
+                                    print(f'{key}: {value}')
+                                print('-' * 50)
                 case 5:
+                    crud.write_json()
                     print('Saindo...')
                     break
 
-        except ValueError as e:
+        except (ValueError, RuntimeError) as e:
             print(e)
 
 if __name__ == "__main__":
