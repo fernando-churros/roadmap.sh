679f82044f248aa7ee1abd57150e06e3af50dd83
fernando-churros <fernando_churros@outlook.com>
2026-09-15 17:41:37 -0300

    feat(task-tracker): adicionar telas de remoção, atualização e listagem no Menu

--- python/task-tracker/Menu.py
+++ python/task-tracker/Menu.py
@@ -6,7 +6,7 @@ class Menu:
         print('-' * qtd)
 
     def initial(self) -> int:
-        self.menu('1 Adicionar | 2 Atualizar | 3 Remover | 4 Listar | 5 Sair', 60)
+        self.menu('1 Adicionar | 2 Remover | 3 Atualizar | 4 Listar | 5 Sair', 60)
 
         x = int(input('Opção: '))
         if not(1 <= x <= 5):
@@ -16,3 +16,12 @@ class Menu:
 
     def create(self):
         self.menu('Adicionando nova tarefa', 30)
+
+    def delete(self):
+        self.menu('Removendo tarefa', 30)
+
+    def update(self):
+        self.menu('Atualizando tarefa\n1 todo | 2 in-progress | 3 done', 30)
+
+    def to_list(self):
+        self.menu('Listar Tarefa\n 1 - Busca única | 2 - Listagem completa', 50)
