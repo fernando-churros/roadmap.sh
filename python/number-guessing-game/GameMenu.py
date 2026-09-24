8d0ea954677a75118bf733dbc7a1c742e7a7d1ce
fernando-churros <fernando_churros@outlook.com>
2026-09-24 04:45:28 -0300

    feat(number-guessing): implementa apresentação e seleção de dificuldades

---
+++ python/number-guessing-game/GameMenu.py
@@ -0,0 +1,41 @@
+class GameMenu:
+    def __init__(self):
+        self.game_name = 'Number Guessing Game' 
+        self.phrase = 'Estou pensando em um número entre 1 e 100. É capaz de adivinhar ?'
+        self.separator = '-'
+        self.line = len(self.phrase)
+        self.difficulty = {
+            '0': ('Unknown Difficulty', 0),
+            '1': ('Easy', 7),
+            '2': ('Medium', 4),
+            '3': ('Hard', 2),
+        }
+
+    def welcome(self):
+        print(self.separator * self.line)
+        print(f'{self.game_name:^{self.line}}')
+        print(self.separator * self.line)
+
+        print(self.phrase)
+        print(self.separator * self.line)
+
+    def select_difficulty(self):
+        phrase_difficulty = 'difficulty -->'
+        for key, value in self.difficulty.items():
+            phrase_difficulty += f' {key} - {value[0]} [{value[1]} Chances] |'
+
+        print(f'{phrase_difficulty} 0 - Sair do Jogo')
+        try:
+            difficulty = str(input('Selecione a difuculdade: '))
+
+            if int(difficulty) == 0:
+                print('Saindo do Jogo...')
+                return self.difficulty[difficulty][0], self.difficulty[difficulty][1]
+
+            if not ( 0 < int(difficulty) <= 4 ): raise ValueError()
+
+            print('Iniciando o jogo\n'.center(30))
+            return self.difficulty[difficulty][0], self.difficulty[difficulty][1]
+        except ValueError as e:
+            print('Dificuldade inválida')
+            return 'Unknown Difficulty', 0
