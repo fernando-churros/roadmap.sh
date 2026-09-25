83e183adb550e22a67182ad458c775c91fa79e00
fernando-churros <fernando_churros@outlook.com>
2026-09-25 02:32:30 -0300

    feat(number-guessing): implementa partidas contínuas e valida palpites

--- python/number-guessing-game/__main__.py
+++ python/number-guessing-game/__main__.py
@@ -1,22 +1,46 @@
+import os
+from time import sleep
 from SecretNumber import SecretNumber
 from GameMenu import GameMenu
 
 def main():
     menu = GameMenu()
-    secret_number = SecretNumber()
 
-    menu.welcome()
-    (difficulty_name, attempt_limit) = menu.select_difficulty()
+    while True:
+        sleep(1)
+        os.system('cls' if os.name == 'nt' else 'clear')
 
-    if attempt_limit == 0: return
+        menu.welcome()
 
-    print(f'DIFICULDADE: {difficulty_name}')
-    for i in range(1, attempt_limit + 1):
-        print(f'Tentativa Nº{i}')
-        try:
-            answer_user = int(input('Qual é o seu palpite: '))
-        except ValueError as e:
-            print('Escolha números inteiros dentro do range.')
+        (difficulty_name, attempt_limit) = menu.select_difficulty()
+
+        if attempt_limit == 0: return
+
+        if attempt_limit == -1:
+            continue
+
+        print(f'DIFICULDADE: {difficulty_name}')
+        secret_number = SecretNumber()
+
+        i = 1
+        while i <= attempt_limit:
+            print(f'Tentativa Nº{i}')
+            try:
+                answer_user = int(input('Qual é o seu palpite: '))
+
+                if not (1 <= answer_user <= 100):
+                    print('Apenas números entre 1 e 100!')
+                    continue
+
+                if secret_number == answer_user:
+                    menu.points = difficulty_name
+                    break
+
+                i += 1
+            except ValueError as e:
+                print('Escolha números inteiros dentro do range.')
+        else:
+            print('Você perdeu')
 
 if __name__ == '__main__':
     main()
