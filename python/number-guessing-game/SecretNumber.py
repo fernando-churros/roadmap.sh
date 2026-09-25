b5260519f31c680b3c92cad1e2eb334799aeb84e
fernando-churros <fernando_churros@outlook.com>
2026-09-25 02:31:23 -0300

    feat(number-guessing): implementa dicas para os palpites

--- python/number-guessing-game/SecretNumber.py
+++ python/number-guessing-game/SecretNumber.py
@@ -4,7 +4,21 @@ class SecretNumber:
     def __init__(self):
         self.__random_num = randint(1, 100)
 
+    @property
+    def random_num(self):
+        return self.__random_num
+
+
     def __eq__(self, other):
-        if self.__random_num == other:
-            return True
-        return False
+        is_win = False
+
+        if self.random_num == other:
+            status = 'Você venceu, parabéns!'
+            is_win = True
+        elif self.random_num > other:
+            status = 'Eu chutaria um número maior.'
+        else:
+            status = 'Eu chutaria um número menor.'
+
+        print(status)
+        return is_win
